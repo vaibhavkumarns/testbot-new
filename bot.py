948a5e0745bef15c81fdee6c82ca4c277545fd7d
vaibhavkumarns <53291967+vaibhavkumarns@users.noreply.github.com>
2021-06-09 18:44:53 +0530

    Initial commit

---
+++ bot.py
@@ -0,0 +1,18 @@
+from botbuilder.core import ActivityHandler, TurnContext
+from botbuilder.schema import ChannelAccount
+
+
+class MyBot(ActivityHandler):
+
+    async def on_message_activity(self, turn_context: TurnContext):
+        print(turn_context.activity.text)
+        await turn_context.send_activity(f"You said '{ turn_context.activity.text }'")
+
+    async def on_members_added_activity(
+        self,
+        members_added: ChannelAccount,
+        turn_context: TurnContext
+    ):
+        for member_added in members_added:
+            if member_added.id != turn_context.activity.recipient.id:
+                await turn_context.send_activity("Hello! What can I do for you?")
