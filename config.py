d7cd17010b644af7fe4e1dbf0424351a58bd0ad5
vaibhavkumarns <53291967+vaibhavkumarns@users.noreply.github.com>
2021-06-10 10:23:14 +0530

    added App ID and password

--- config.py
+++ config.py
@@ -3,8 +3,7 @@ import os
 
 class DefaultConfig:
     PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
-
-    # APP_ID = os.environ.get("MicrosoftAppId", "06e03ae2-39b1-4c2d-bb92-88a9d82479aa")
-    # APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "Zpq3~phWY653KMwCu.zxWwDy3MIC2--Y7n")
+    # APP_ID = os.environ.get("MicrosoftAppId", "")
+    # APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    APP_ID = os.environ.get("MicrosoftAppId", "06e03ae2-39b1-4c2d-bb92-88a9d82479aa")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "Zpq3~phWY653KMwCu.zxWwDy3MIC2--Y7n")
